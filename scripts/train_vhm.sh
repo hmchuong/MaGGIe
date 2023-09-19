82973b185aec79cf3c1bcf498846d95dca57d452
chuongh <chuongh@adobe.com>
2023-09-19 21:33:33 +0000

    [UPDATE] train new VHM 0919

--- scripts/train_vhm.sh
+++ scripts/train_vhm.sh
@@ -11,7 +11,7 @@ sh scripts/prepare_vhm_syn.sh
 # Start training
 echo "Starting training..."
 cd $ROOT_DIR
-CONFIG=configs/VideoMatte240K/ours_vhm_0905.yaml
+CONFIG=configs/VideoMatte240K/ours_vhm_0919.yaml
 # TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=$RUNAI_NUM_OF_GPUS --nnodes=$WORLD_SIZE --node_rank=$RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT tools/main_ddp.py \
 #                     --config $CONFIG --precision 16 model.weights output/VHM/ours_vhm_0905/best_model.pth
 
